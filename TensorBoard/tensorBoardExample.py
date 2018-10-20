e95fd22688d4d1d0bd340d11ad4f7faa66410585
SaretMagnoslove <magnoslove@gmail.com>
2018-10-20 13:49:55 +0300

    Add Optimizing with TensorBoard

--- TensorBoard/tensorBoardExample.py
+++ TensorBoard/tensorBoardExample.py
@@ -4,44 +4,47 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.callbacks import TensorBoard
-import time
 import pickle
 
-# Naming the model and adding a time stump
-
-NAME = "Cats-vs-dogs-CNN-No-Dense"
-# Adding TensorBoard
-
-tensorboard = TensorBoard(log_dir=f'logs/{NAME}')
-
 # # Loading and Normalizing Data
-
 X = pickle.load(open('X.pickle', 'rb'))
 y = pickle.load(open('y.pickle', 'rb'))
 
 X = X/255.00
 
-# # Building the Model
+# Naming the models
+dense_layers = [0,1,2]
+layer_sizes = [32,64,128]
+conv_layers = [1,2,3]
 
-model = Sequential()
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = f"{conv_layer}-conv-{layer_size}-nodes-{dense_layer}-dense"
+            # Adding TensorBoard
+            tensorboard = TensorBoard(log_dir=f'logs/{NAME}')
+            # Building the Model
+            model = Sequential()
 
-model.add(Conv2D(64, (3,3), input_shape=X.shape[1:]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+            model.add(Conv2D(layer_size, (3,3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(64, (3,3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3,3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Flatten())
-# model.add(Dense(64))
-# model.add(Activation('relu'))
+            model.add(Flatten())
+            for l in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
 
-model.compile(loss='binary_crossentropy',
-             optimizer='adam',
-             metrics=['accuracy'])
+            model.compile(loss='binary_crossentropy',
+                        optimizer='adam',
+                        metrics=['accuracy'])
 
-model.fit(X, y, batch_size=32, epochs=10, validation_split=0.3, callbacks=[tensorboard])
+            model.fit(X, y, batch_size=32, epochs=10, validation_split=0.3, callbacks=[tensorboard])
